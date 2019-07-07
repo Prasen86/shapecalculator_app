99b99248d5545610030c67b357fe2b1ea34c21a5
Rhythm Solutions <rhythmsolutions2017@gmail.com>
2019-07-07 18:56:06 +0530

    Adding Home Screen

--- lib/calcScreen.dart
+++ lib/calcScreen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:shapecalculator_app/calcScreen.dart';
 
-class ScreenApp extends StatelessWidget {
+class CalcApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.red,
+        primaryColor: Colors.green,
         brightness: Brightness.light,
       ),
       home: ScreenPage(),
@@ -20,26 +19,11 @@ class ScreenPage extends StatefulWidget {
 }
 
 class _ScreenPageState extends State<ScreenPage> {
-
-  //Navigate to Calculator Screen
-  void _navigateToCalcScreen() {
-    Navigator.push(context, MaterialPageRoute(
-      builder: (context) {
-        return CalcApp();
-      },
-    ));
-  }
-
-  //Body of the HOMESCREEN
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("HOME"),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _navigateToCalcScreen,
-        child: new Icon(Icons.add_alarm),
       ),
     );
   }
